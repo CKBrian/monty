b611df0d20e1d045eb7ccfc6a4da8e2f42cec801
Brian <chumobrian@gmail.com>
2023-09-21 02:20:31 +0300

    feat: Add a function that does nothing

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+ * nop_stack - does nothing
+ * @line_number: integer
+ * @stack: head node of stack
+ * Return: a new node
+ */
+void nop_stack(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
