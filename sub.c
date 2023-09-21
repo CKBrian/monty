da0d1163a49cd7f466251a4b27ad69818c257629
Robert Odhiambo <odhizrober254@gmail.com>
2023-09-21 18:31:19 +0000

    added sub.c

---
+++ sub.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+/**
+ * sub_stack - subs top two elements of a  stack
+ * @line_number: integer
+ * @stack: head node of stack
+ * Return: a new node
+ */
+void sub_stack(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp = *stack;
+	int first, second;
+
+	if (temp == NULL || temp->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+		free_stack(*stack);
+		fclose(data_lib->file);
+		free(data_lib);
+		exit(EXIT_FAILURE);
+	}
+	first = (*stack)->n;
+	second = temp->next->n;
+	temp = temp->next;
+	free(*stack);
+	temp->n = second - first;
+	*stack = temp;
+	temp->prev = NULL;
+
+}
