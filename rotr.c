51377d1ff5553be9d11c5e09cd709657d14c61d1
Brian <chumobrian@gmail.com>
2023-09-23 14:10:17 +0300

    feat: Add a functiom that moves the bottom stack element to the top of the stack

--- rotr.c
+++ rotr.c
@@ -1,26 +1,26 @@
 #include "monty.h"
 /**
- * rotr_stack - inverts the stack making top element to be the last 
+ * rotr_stack - inverts the stack making top element to be the last
  * @line_number: integer
  * @stack: stack node of stack
  * Return: a new node
  */
 void rotr_stack(stack_t **stack, unsigned int line_number)
 {
-	stack_t *ptemp = NULL, *ntemp = *stack;
-
+	stack_t  *temp = *stack, *second = NULL;
 	(void)line_number;
-	if (*stack == NULL)
+	if (temp == NULL || temp->next == NULL)
 		return;
-	while (ntemp->next != NULL)
-	{
-		ntemp = ntemp->next;
-		(*stack)->next = ptemp;
-		(*stack)->prev = ntemp;
-		ptemp = *stack;
-		*stack = ntemp;
-	}
-	(*stack)->next = ptemp;
-	(*stack)->prev = NULL;
 
+	while (temp->next->next)
+		temp = temp->next;
+	second = temp;
+	temp = temp->next;
+	temp->next = *stack;
+	(*stack)->prev = temp;
+	temp->prev = NULL;
+
+	*stack = temp;
+	if (second)
+		second->next = NULL;
 }
